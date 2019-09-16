b56f1af00b0d60cc1e20afc41eb5e9f59b75cfca
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-16 22:46:15 +0200

    lesson4 speech added after photo

--- Vision-app-dev/Controller/CameraVC.swift
+++ Vision-app-dev/Controller/CameraVC.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
+enum FlashState {
+    case off
+    case on
+}
 class CameraVC: UIViewController {
     
     var captureSession: AVCaptureSession!
-    var cameraOutput: AVCaptureOutput!
+    var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var photoData: Data?
+    
+    var flashControlState: FlashState = .off
+    
+    var speechSynthesizer = AVSpeechSynthesizer()
     
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
     @IBOutlet weak var flashBtn: RoundedShadowButton!
@@ -21,6 +33,7 @@ class CameraVC: UIViewController {
     @IBOutlet weak var confidenceLbl: UILabel!
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var roundedLblView: RoundedShadowView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +42,17 @@ class CameraVC: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         previewLayer.frame = cameraView.bounds
+        speechSynthesizer.delegate = self
+        spinner.hidesWhenStopped = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
+        cameraView.addGestureRecognizer(tap)
+        
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
         
@@ -60,12 +80,91 @@ class CameraVC: UIViewController {
         }
     }
 
-    func didTapCameraView() {
+    @objc func didTapCameraView() {
+        self.cameraView.isUserInteractionEnabled = false
+        spinner.startAnimating()
+        
         let settings = AVCapturePhotoSettings()
-        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!  
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        
+        if flashControlState == .off {
+            settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func resultMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            if classification.confidence < 0.5 {
+                let unknownObjectMessage = "I'am not sure what this is. Please try again"
+                self.identificationLbl.text = unknownObjectMessage
+                synthesizerSpeech(fromString: unknownObjectMessage)
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                let identification = classification.identifier
+                let confidence = "\(Int(classification.confidence * 100))"
+                self.identificationLbl.text = identification
+                self.confidenceLbl.text = "CONFIDENCE: \(confidence)%"
+                
+                let completeSentence = "this looks like a \(identification) and I'm \(confidence) percent sure"
+                synthesizerSpeech(fromString: completeSentence)
+
+                break
+            }
+        }
+    }
+    
+    func synthesizerSpeech(fromString string: String) {
+        let speechUtterance = AVSpeechUtterance(string: string)
+        speechSynthesizer.speak(speechUtterance)
+    }
+    
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashControlState {
+        case .off:
+            flashBtn.setTitle("FLASH ON", for: .normal)
+            flashControlState = .on
+        case .on:
+            flashBtn.setTitle("FLASH OFF", for: .normal)
+            flashControlState = .off
+        }
     }
 }
 
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+                
+            } catch {
+                debugPrint(error)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+    }
+}
+
+extension CameraVC: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        
+    }
+}
 
 
 
